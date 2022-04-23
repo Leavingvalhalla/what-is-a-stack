c02ee3e7a7feb1c84501748ee12f0907e75262e8
Zach Lefler <leaving.valhalla@gmail.com>
2022-04-23 16:24:59 -0700

    initial commit

--- ruby/stack.rb
+++ ruby/stack.rb
@@ -11,35 +11,49 @@ class Stack
   # add item to top of stack if not full
   # if full, throw error
   def push(item)
+    if self.stack.length < self.limit
+      self.stack.push(item)
+    else
+      raise IndexError.new 'Stack already full'
+    end
   end
 
-  # remove item from top of stack and return it
+  
   def pop
+    self.stack.pop
   end
 
-  # return item at top of stack without removing it
+  
   def peek
+    self.stack[-1]
   end
 
   # return true if stack is empty, otherwise false
   def isEmpty?
+    self.stack == []
   end
 
   # return true if stack is full, otherwise false
   def isFull?
+    self.stack.length == self.limit
   end
 
   # return number of items in stack
   def size
+    self.stack.length
   end
 
   # return -1 if item not in stack, otherwise integer representing 
   # how far it is from the top
   def search(target)
+    x = self.stack.index(target)
+    return -1 if x == nil
+    self.stack.length - x - 1
   end
 
   # print contents of stack: do not return the stack itself!
   def print
+    self.stack[0..-1]
   end
 end
 
